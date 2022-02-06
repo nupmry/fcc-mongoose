e42f011986fcf4a6487822d7c22a38302369dab6
Anupam Arya <anu9am@gmail.com>
2022-02-06 19:36:21 +0000

    challenge 6

--- myApp.js
+++ myApp.js
@@ -46,7 +46,12 @@ const findPeopleByName = (personName, done) => {
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({favoriteFoods : food}, (err, data) => {
+    if(err){
+      return done(err);
+    }
+      done(null, data);
+  })
 };
 
 const findPersonById = (personId, done) => {
